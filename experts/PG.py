715b67d6b21856823306919d8352a570627ab2e6
Nishant Kumar <nishantkr.ece18@itbhu.ac.in>
2020-10-08 15:16:57 +0530

    Added PG as an expert for generating demos

---
+++ experts/PG.py
@@ -0,0 +1,82 @@
+import numpy as np
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+class PG(nn.Module):
+    def __init__(self, state_shape, n_actions):
+        
+        super().__init__()
+        self.state_shape = state_shape
+        self.n_actions = n_actions
+        self.model = nn.Sequential(
+            nn.Linear(in_features = state_shape[0], out_features = 128),
+            nn.ReLU(),
+            nn.Linear(in_features = 128 , out_features = 64),
+            nn.ReLU(),
+            nn.Linear(in_features = 64 , out_features = 2)
+        )
+        self.optimizer = torch.optim.Adam(self.model.parameters(), 1e-3)
+    def forward(self, x):
+        logits = self.model(x)
+        return logits
+    
+    def predict_probs(self, states):
+        states = torch.FloatTensor(states)
+        logits = self.model(states).detach()
+        probs = F.softmax(logits, dim = -1).numpy()
+        return probs
+    
+    def generate_session(self, env, t_max=1000):
+        states, actions, rewards = [], [], []
+        s = env.reset()
+
+        for t in range(t_max):
+            action_probs = self.predict_probs(np.array([s]))[0]
+            a = np.random.choice(self.n_actions,  p = action_probs)
+            new_s, r, done, info = env.step(a)
+
+            states.append(s)
+            actions.append(a)
+            rewards.append(r)
+
+            s = new_s
+            if done:
+                break
+
+        return states, actions, rewards
+
+    def _get_cumulative_rewards(self, rewards, gamma=0.99):
+        G = np.zeros_like(rewards, dtype = float)
+        G[-1] = rewards[-1]
+        for idx in range(-2, -len(rewards)-1, -1):
+            G[idx] = rewards[idx] + gamma * G[idx+1]
+        return G
+
+    def _to_one_hot(self, y_tensor, ndims):
+        y_tensor = y_tensor.type(torch.LongTensor).view(-1, 1)
+        y_one_hot = torch.zeros(
+            y_tensor.size()[0], ndims).scatter_(1, y_tensor, 1)
+        return y_one_hot
+
+    def train_on_env(self, env, gamma=0.99, entropy_coef=1e-2):
+        states, actions, rewards = self.generate_session(env)
+        states = torch.tensor(states, dtype=torch.float32)
+        actions = torch.tensor(actions, dtype=torch.int32)
+        cumulative_returns = np.array(self._get_cumulative_rewards(rewards, gamma))
+        cumulative_returns = torch.tensor(cumulative_returns, dtype=torch.float32)
+
+        logits = self.model(states)
+        probs = nn.functional.softmax(logits, -1)
+        log_probs = nn.functional.log_softmax(logits, -1)
+
+        log_probs_for_actions = torch.sum(
+            log_probs * self._to_one_hot(actions, env.action_space.n), dim=1)
+    
+        entropy = -torch.mean(torch.sum(probs*log_probs), dim = -1 )
+        loss = -torch.mean(log_probs_for_actions*cumulative_returns -entropy*entropy_coef)
+
+        loss.backward()
+        self.optimizer.step()
+        self.optimizer.zero_grad()
+        return np.sum(rewards)
---
+++ generating_expert.py
@@ -0,0 +1,53 @@
+import gym
+import random
+import numpy as np
+import matplotlib.pyplot as plt
+import torch
+
+from experts.PG import PG
+
+from torch.optim.lr_scheduler import StepLR
+
+seed = 18095048
+random.seed(seed)
+np.random.seed(seed)
+torch.manual_seed(seed)
+
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+env_name = 'CartPole-v1'
+env = gym.make(env_name).unwrapped
+if seed is not None:
+    env.seed(seed)
+state = env.reset()
+
+n_actions = env.action_space.n
+state_shape = env.observation_space.shape
+
+# initializing a model
+model = PG(state_shape, n_actions)
+
+mean_rewards = []
+for i in range(100):
+    rewards = [model.train_on_env(env) for _ in range(100)] 
+    mean_rewards.append(np.mean(rewards))
+    if i % 5:
+        print("mean reward:%.3f" % (np.mean(rewards)))
+        plt.figure(figsize=[9, 6])
+        plt.title("Mean reward per 100 games")
+        plt.plot(mean_rewards)
+        plt.grid()
+        # plt.show()
+        plt.savefig('plots/PG_learning_curve.png')
+        plt.close()
+    
+    if np.mean(rewards) > 500:
+        break
+
+torch.save(model, "experts/saved_expert/pg.model")
+#model.load("experts/saved_expert/pg.model")
+
+num_demo = 500
+
+expert_samples = np.array([model.generate_session(env) for i in range(num_demo)])
+np.save('expert_samples/pg_cartpole', expert_samples)
